7b9b4182c1f3bcba401097b46ebc1f458c31171d
atuls9 <atul.sooryawanshi@gmail.com>
2023-02-21 18:30:11 +0530

    getElementById and DOM task 3 done

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 //*****EXAMINE THE DOCUMENT OBJECT*****
-
+// console.dir(document);
 // console.log(document);
 // console.log(document.domain);
 // console.log(document.URL);
 // console.log(document.title); 
-console.log(document)
+// document.title = "dom";
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.forms[0][1]);
+// console.log(document.links);
+// --------------------------------------------------------------
+
+//***** GETELEMENTBUID *****//
+// console.log(document.getElementById('header-title'))
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.textContent="hello";
+// headerTitle.innerText = "Goodbye";
+// headerTitle.innerHTML= "<h3>Hello</h3>"
+
+// var headerTitle = document.getElementById('main-header');
+// headerTitle.style.borderBottom = "solid 3px #000"
+
+//adding id = subtitle for Add Items title
+var subHeading = document.getElementById('subtitle');
+console.log(subHeading)
+// subHeading.textContent = 'ADD ITEM';
+// subHeading.style.fontWeight = 'bold';
+// subHeading.style.color = 'green';
 
